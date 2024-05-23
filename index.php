076ed3a48b169319d8d08327ff4154495732bf75
Andrew Hinkson <adh1056@comcast.net>
2024-05-23 13:31:56 -0400

    added list with students by last, first name

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+echo "<h1>Student List</h1>";
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/../config.php';
+
+try
+{
+    //Instantiate our PDO DB object
+    $dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+    echo 'Connected to database!';
+}
+catch (PDOException $e)
+{
+    die( $e->getMessage() );
+}
+
+//SELECT MULTIPLE ROWS QUERY
+//1. Define the Query
+$sql = "SELECT * FROM student";
+
+//2. Prepare the statement
+$statement = $dbh->prepare($sql);
+
+//3. Bind the parameters (none here)
+
+//4. Execute the statement
+$statement->execute();
+
+//Process the result
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+foreach ($result as $row)
+{
+    echo "<p>".$row['last'].", ".$row['first']."</p>";
+}
